a8f6c70cdafeedcbfe444a7f7a75af09b5ba0e33
blacksfk <blacksunflowakuttaspam@hotmail.com>
2021-03-30 19:15:11 +1100

    weather and track status

---
+++ src/components/Conditions.vue
@@ -0,0 +1,73 @@
+<template>
+<div>
+	<div class="conditions-wind-container">
+		<div class="conditions-wind-direction" :style="rotation">&#129047;</div>
+		<h3>{{ windSpeed }}m/s</h3>
+	</div>
+	<div class="table-responsive">
+		<table class="table">
+			<tbody>
+				<tr>
+					<th>Ambient</th>
+					<td>{{ temp.ambient }}&#8451;</td>
+				</tr>
+				<tr>
+					<th>Track</th>
+					<td>{{ temp.track }}&#8451;</td>
+				</tr>
+				<tr>
+					<th>Track status</th>
+					<td>{{ track }}</td>
+				</tr>
+				<tr>
+					<th>Rain Intensity now</th>
+					<td>{{ rain.current }}</td>
+				</tr>
+				<tr>
+					<th>Rain Intensity in 10 minutes</th>
+					<td>{{ rain.in10 }}</td>
+				</tr>
+				<tr>
+					<th>Rain Intensity in 30 minutes</th>
+					<td>{{ rain.in30 }}</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
+</template>
+<style>
+.conditions-wind-container {
+	display: flex;
+	flex-flow: column wrap;
+	align-items: center;
+}
+
+.conditions-wind-direction {
+	will-change: transform;
+	transition: transform 0.1s;
+	font-size: 200%;
+}
+</style>
+<script>
+let props = {
+	windDirection: 0,
+	windSpeed: 0,
+	temp: Object,
+	track: String,
+	rain: Object
+}
+
+let computed = {
+	rotation() {
+		return {
+			transform: `rotateZ(${this.windDirection}rad)`
+		};
+	}
+};
+
+export default {
+	props,
+	computed
+};
+</script>
