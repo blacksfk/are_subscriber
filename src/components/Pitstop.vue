262a32065efb3a7b8488834e021caedc29a0ff72
blacksfk <blacksunflowakuttaspam@hotmail.com>
2021-03-30 15:08:29 +1100

    next pit stop

---
+++ src/components/Pitstop.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="table-responsive">
+	<table class="table">
+		<tbody>
+			<tr>
+				<th>Tyre set</th>
+				<td>{{ tyreSet }}</td>
+			</tr>
+			<tr>
+				<th>Fuel fill</th>
+				<td>{{ fuel }}L</td>
+			</tr>
+			<tr>
+				<th>FL</th>
+				<td>{{ pressure.fl }}psi</td>
+			</tr>
+			<tr>
+				<th>FR</th>
+				<td>{{ pressure.fr }}psi</td>
+			</tr>
+			<tr>
+				<th>RL</th>
+				<td>{{ pressure.rl }}psi</td>
+			</tr>
+			<tr>
+				<th>RR</th>
+				<td>{{ pressure.rr }}psi</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+</template>
+<script>
+let props = {
+	tyreSet: Number,
+	fuel: Number,
+	pressure: Object
+};
+
+export default {
+	props
+};
+</script>
