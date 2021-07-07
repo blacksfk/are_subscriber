1040d148c7e1638f60e30c9a0de9c7c4890f2d56
blacksfk <blacksunflowakuttaspam@hotmail.com>
2021-07-07 23:12:17 +1000

    created tab row component

---
+++ src/components/TabRow.vue
@@ -0,0 +1,73 @@
+<template>
+<div class="tab-row">
+	<div class="tab-row-item" v-for="(item, i) in items" :class="{'tab-row-item-active': value === i}" @click="emit(i)">{{ item }}</div>
+</div>
+</template>
+<style>
+:root {
+	--tab-row-border-style: 1px solid var(--primary);
+}
+
+.tab-row {
+	display: flex;
+	flex-flow: row wrap;
+	width: 100%;
+	background-color: var(--bg-primary);
+	border-bottom: var(--tab-row-border-style);
+}
+
+.tab-row-item {
+	padding: 10px;
+	text-align: center;
+	/* negative margin to hide .tab-row bottom border on currently selected tab */
+	margin-bottom: -1px;
+}
+
+.tab-row-item:hover {
+	cursor: pointer;
+}
+
+.tab-row-item:first-child {
+	margin-left: 10px;
+}
+
+.tab-row-item-active {
+	border: var(--tab-row-border-style);
+	border-bottom-color: var(--bg-primary);
+	border-radius: 4px 4px 0 0;
+}
+
+.tab-row-item-active:hover {
+	cursor: default;
+}
+</style>
+<script>
+/**
+ * Injected properties.
+ */
+let props = {
+	/**
+	 * The currently selected tab.
+	 */
+	value: Number,
+
+	/**
+	 * Array of strings to be used for the tab names.
+	 */
+	items: Array
+};
+
+/**
+ * Event emitter. Emits an "input" event when a tab is clicked.
+ * @param  {Number} i
+ * @return {void}
+ */
+function emit(i) {
+	this.$emit("input", i);
+}
+
+export default {
+	props,
+	methods: {emit}
+};
+</script>
