a9acea43d1827240994f394ba2226fa3366acd7c
blacksfk <blacksunflowakuttaspam@hotmail.com>
2021-08-29 00:21:23 +1000

    $ajaxWithAuth plugin

--- src/plugins/ajax.js
+++ src/plugins/ajax.js
@@ -1,9 +1,30 @@
 import Axios from "axios";
 
 function install(app, config) {
-	let axios = Axios.create(config);
+	let withAuth = Axios.create(config);
+	let auth = app.config.globalProperties.$auth;
 
-	app.config.globalProperties.$ajax = axios;
+	// intercept requests and add an
+	// "Authorization" header
+	withAuth.interceptors.request.use(conf => {
+		let base = conf.baseURL;
+		let path = conf.url;
+
+		if (path[0] !== "/") {
+			// prefix the path
+			path = `/${path}`;
+		}
+
+		// generate an authorization header
+		// and add it to the config
+		conf.headers.Authorization = auth.header(
+			`${base}${path}`, conf.method);
+
+		return conf;
+	});
+
+	app.config.globalProperties.$ajaxWithAuth = withAuth;
+	app.config.globalProperties.$ajax = Axios.create(config);
 }
 
 export default {
